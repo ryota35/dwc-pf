f680c5350ace18ba34be6a577f673326d9e3cc59
ryota35 <ryota35.1025@gmail.com>
2021-04-21 18:00:26 +0000

    下からふわっと出るjquery試してみた

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -66,3 +66,17 @@ $(function(){
     $(this).text('B S L');
   });
 });
+
+
+$(function(){
+    $(window).scroll(function (){
+        $('.fadein').each(function(){
+            var elemPos = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > elemPos - windowHeight + 200){
+                $(this).addClass('scrollin');
+            }
+        });
+    });
+});
