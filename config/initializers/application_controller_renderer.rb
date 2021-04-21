bad4ff1c38f7bece7d24dd7a32cc0ba2ac72e215
ryota35 <ryota35.1025@gmail.com>
2021-04-21 11:32:28 +0000

    cssのbackground-imageが本番環境で表示されなかったのでcssを修正

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -4,3 +4,5 @@
 #   http_host: 'example.org',
 #   https: false
 # )
+
+Refile.secret_key = 'a8590661c5ca3a27cfd80d71452e12c1b930fc7b5c309a858e8c6fee4b2c43a264a79195a383dc78b8489dcecc1601b011645b35776d48e2c27d25094135fd7d'
