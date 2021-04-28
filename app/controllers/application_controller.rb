50fbcd0c2582db87bb7c620d70313b6a00448f16
ryota35 <ryota35.1025@gmail.com>
2021-04-28 16:04:30 +0000

    会員登録時のバグを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters,if: :devise_controller?
 
   def after_sign_in_path_for(resource)
     case resource
@@ -13,5 +14,10 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
     root_path
   end
+
+  private
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up,keys:[:name])
+    end
 
 end
