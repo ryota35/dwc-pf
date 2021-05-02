a1e6b6245429b4ea8b432305bdb76e8765814443
ryota35 <ryota35.1025@gmail.com>
2021-05-02 09:29:37 +0000

    vision-apiをとりあえず実装完了、デバッグ開始前

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -34,11 +34,15 @@ class Public::UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    
+
     if @user.update(user_params)
-      tags = Vision.get_image_data(@user.itemimage)
-      tags.each do |tag|
-        @user.itemimage_tags.create(name: tag)
+      @user.itemimage_tags.destroy_all
+      if @user.itemimage_id.balnk?
+      else
+        tags = Vision.get_image_data(@user.itemimage)
+        tags.each do |tag|
+          @user.itemimage_tags.create(name: tag)
+        end
       end
       redirect_to user_path(@user)
     else
